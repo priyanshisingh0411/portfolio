54ca7d4d84873e216b07562e63fa54b8346da2b1
Priyanshi Singh <singhpriyanshi63@gmail.com>
2025-09-23 15:34:18 +0530

    changes next.config

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,10 @@ const nextConfig: NextConfig = {
   /* config options here */
   output: "export",
   basePath: "/portfolio",
+  assetPrefix: "/portfolio",
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
