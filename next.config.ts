de82988c75cf78269ed7b7b09ca06e857f460735
Priyanshi Singh <singhpriyanshi63@gmail.com>
2026-06-24 14:56:18 +0530

    next.config changes

--- next.config.ts
+++ next.config.ts
@@ -3,8 +3,8 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   output: "export",
-  // basePath: "/portfolio",
-  // assetPrefix: "/portfolio",
+  basePath: "/portfolio",
+  assetPrefix: "/portfolio",
   images: {
     unoptimized: true,
   },
