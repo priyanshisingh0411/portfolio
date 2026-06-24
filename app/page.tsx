d77a0a313f7df827328a233398e3a7c34e6266cc
Priyanshi Singh <singhpriyanshi63@gmail.com>
2026-06-24 15:02:12 +0530

    next.config changes

--- app/page.tsx
+++ app/page.tsx
@@ -13,7 +13,7 @@ import { navItems } from "@/data";
 
 export default function Home() {
   return (
-    <main className="relative bg-black-100 flex justify-center items-center flex-col mx-auto sm:px-10 px-5">
+    <main className="relative bg-black-100 overflow-hidden flex justify-center items-center flex-col mx-auto sm:px-10 px-5">
       <div className="max-w-7xl w-full">
         <FloatingNav navItems={navItems} />
         <Profile />
